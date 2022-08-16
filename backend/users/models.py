77e4193c605721aa014ddb3b951d029e826bba69
Jacqueline <jhjzhou@gmail.com>
2022-08-16 17:33:54 -0400

    adding in test files

--- backend/users/models.py
+++ backend/users/models.py
@@ -6,4 +6,4 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     username = models.CharField(unique=True, max_length=30)
     email = models.CharField(unique=True, max_length=50)
-    avatar = models.URLField(default='image', max_length=2000)
+    avatar = models.URLField(default='https://thenounproject.com/icon/perfume-80945/', max_length=2000)
--- backend/users/tests/tests.py
+++ backend/users/tests/tests.py
@@ -1,10 +1,30 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase
+from rest_framework.test import APITestCase
+User = get_user_model()
 
-class TestUsers(TestCase):
-    def test_create_user(self):
-        User = get_user_model()
+class TestUsers(APITestCase):
+    def setUp(self) -> None:
+        new_user = User.objects.create(username = 'user1', email = 'emails@email.com')
+        new_user.set_password('passing')
+
+    def test_create_user(self) -> None:
         user = User.objects.create(username = 'user123', password='password', email='user@email.com')
         self.assertEqual(user.email, 'user@email.com')
+        self.assertEqual(user.avatar, 'https://thenounproject.com/icon/perfume-80945/')
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_superuser)
+    
+    def test_login(self) -> None:
+        response = self.client.post('/login/', {
+            'username': 'user1',
+            'password': 'passing',
+        })
+        self.assertEqual(response.status_code, 202)
+
+    def test_register(self) -> None:
+        response = self.client.post('/register/', {
+            'username': 'user2',
+            'password': 'passing2',
+            'password_confirmation': 'passing2'
+        })
+        self.assertEqual(response.status_code, 201)
