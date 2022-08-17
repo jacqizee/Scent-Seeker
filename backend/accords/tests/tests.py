edc7289ec30750232b55ae61ee8ce839db0f6a40
Jacqueline <jhjzhou@gmail.com>
2022-08-17 16:23:11 -0400

    added test for accords

--- backend/accords/tests/tests.py
+++ backend/accords/tests/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from ..models import Accord
 # Create your tests here.
+
+class TestAccords(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        Accord.objects.create(name = 'Floral')
+
+    def test_model(self):
+        self.assertTrue(Accord.objects.get(name='Floral'))
