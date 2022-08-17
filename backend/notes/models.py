cdb5c560d3626e185142e5682b9c80ebc864637d
Jacqueline <jhjzhou@gmail.com>
2022-08-17 16:36:43 -0400

    adding testing files to notes, updating model

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -4,3 +4,6 @@ from django.db import models
 
 class Note(models.Model):
     name = models.CharField(max_length=50, unique=True)
+    category = models.ForeignKey('note_categories.Category', on_delete=models.RESTRICT)
+    image = models.URLField(max_length = 2000)
+    description = models.TextField(max_length = 500, blank=True)
